acfc2fca76756e2d19f71eabb935d215faf200c7
Amnhed <aalhe0.0@gmail.com>
2020-11-26 22:58:32 -0600

    Scope

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hola mundo')
+// Scope en variables
+let valorDado = 5
+valorDado = 4
+
+if(true){
+    let valorDado = 6
+    console.log(valorDado)//Imprime 6
+}
+console.log(valorDado)//imprime 4
